d5137f943edf084f05fe87b5240ea46e33b6dd98
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-01-09 19:56:35 +0530

    Github Login

--- server/app/index.js
+++ server/app/index.js
@@ -3,20 +3,40 @@ import http from 'http';
 import fs from 'fs';
 import compress from 'compression';
 import config from '../../settings/dev';
+import fetch from 'node-fetch';
 
 //import DebugMiddleware from './app/middleware/debug';
 
 let app = express();
 
-var client_id = config.client_id, redirect_uri = config.redirect_uri, scope ='' , state = 'pika';
+var client_id = config.client_id, redirect_uri = config.redirect_uri, scope =config.scope , state = config.state;
 
 
 app.get('/',(req,res)=>{
-	res.redirect('https://github.com/login/oauth/authorize?client_id='+client_id);
+	res.redirect('https://github.com/login/oauth/authorize?client_id='+client_id+'&state='+state+'&scope='+scope);
 });
 app.get('/success',(req,res)=>{
-	console.log(res);
-	res.send('success');
+	if(typeof req.query.code !== 'undefined' && req.query.state === config.state ){
+		//res.send(req.query.code);	
+		fetch('https://github.com/login/oauth/access_token',{
+			method: 'POST',
+		      headers: {
+		        'Accept': 'application/json',
+		        'Content-Type': 'application/json'
+		      },
+		      body: JSON.stringify({
+		        client_id: config.client_id,
+		        client_secret: config.client_secret,
+		        code : req.query.code,
+		        state:config.state
+		      })
+		    }).then(response=>response.json()).then(response => {
+		    	res.send(response);
+		    });	
+	}else{
+		res.send(req);
+	}
+
 });
 
 app.get('/fail',(req,res)=>{
